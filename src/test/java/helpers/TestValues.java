43f006bc2e3dc8f76a9beeed515a91eca1de354d
”Sergey <s.polishevskiy@gmail.com>
2022-03-13 18:32:11 +0200

    Updated the email and title values

--- src/test/java/helpers/TestValues.java
+++ src/test/java/helpers/TestValues.java
@@ -3,5 +3,5 @@ package helpers;
 public class TestValues {
     public static String TEST_BODY = "Сообщение в тех поддержку с помощью селениума";
     public static String TEST_TITLE = "Sergey AQA";
-    public static String TEST_EMAIL = "aqa@gmail.com";
+    public static String TEST_EMAIL = "automationqa@gmail.com";
 }
