5659bfc8d3a5105d508ea906e8d71459ddc55b7d
”Sergey <s.polishevskiy@gmail.com>
2022-03-13 18:24:04 +0200

    Helpdesk Case Java+Selenium+JUnit

---
+++ src/test/java/helpers/TestValues.java
@@ -0,0 +1,7 @@
+package helpers;
+
+public class TestValues {
+    public static String TEST_BODY = "Сообщение в тех поддержку с помощью селениума";
+    public static String TEST_TITLE = "Oleh Youtuber";
+    public static String TEST_EMAIL = "testYoutube@fakare.ru";
+}
